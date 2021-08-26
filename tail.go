133fe7fb14fc32c44c7766916c2ad222d58b47bd
Ian Marsman <ian@xmlteam.com>
2021-08-26 19:04:24 -0400

    Mostly finished

---
+++ tail.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+/*
+	This app takes a number of lines argument, a "pretty" argument for more
+	illustrative output, and a list of paths to files, and for each file gathers
+	the number of lines requested, if available, and then prints them out to
+	standard out.
+
+	The ideal implementation would use a buffer to read in just enough of each
+	file to satisfy the number of lines parameter.
+*/
+
+// getLines get lasn num lines in file and return them as a string slice. Return
+// an error if for instance a filename is incorrect.
+func getLines(num int, path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// A bit inefficient as whole file is read in then out again in reverse
+	// order up to num.
+	var all []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		all = append(all, scanner.Text())
+	}
+
+	// Get last num lines
+	var lines []string
+	for i := len(all) - 1; i > -1; i-- {
+		lines = append(lines, all[i])
+		if len(lines) == num {
+			break
+		}
+	}
+
+	// Sort reverse
+	sort.Slice(lines, func(i, j int) bool {
+		return i > j
+	})
+
+	return lines, scanner.Err()
+}
+
+func main() {
+	var h bool
+	flag.BoolVar(&h, "h", false, "print usage")
+	var p bool
+	flag.BoolVar(&p, "pretty", false, "add formatting to output")
+	var n int
+	flag.IntVar(&n, "n", 10, "number of lines")
+	flag.Parse()
+	if h == true {
+		fmt.Println("Print last n lines of one or more files")
+		fmt.Println("Example: tail -n 10 file1.txt file2.txt")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// If a large amount of processing is required handling output for a file at
+	// a time shoud help the garbage collector and memory usage.
+	var write = func(fname string, lines []string) {
+		builder := strings.Builder{}
+		if p == true {
+			builder.WriteString(fmt.Sprintf("%s\n", strings.Repeat("-", 50)))
+		}
+		builder.WriteString(fmt.Sprintf("File %s\n", fname))
+		if p == true {
+			builder.WriteString(fmt.Sprintf("%s\n", strings.Repeat("-", 50)))
+		}
+		for _, v := range lines {
+			builder.WriteString(fmt.Sprintf("%s\n", v))
+		}
+		fmt.Println(strings.TrimSpace(builder.String()))
+	}
+
+	// Iterate through list of files (the bits that are not flags), using a
+	// strings builder to prepare output. Strings builder avoids allocation.
+	// This could be more efficient if the lines were printed immediately.
+	for _, v := range flag.Args() {
+		lines, err := getLines(n, v)
+		if err != nil {
+			// panic if something like a bad filename is used
+			panic(err)
+		}
+		write(v, lines)
+	}
+}
---
+++ tail_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"testing"
+)
+
+//                Tests and benchmarks
+// -----------------------------------------------------
+// benchmark
+//   go test -run=XXX -bench=. -benchmem
+// Get allocation information and pipe to less
+//   go build -gcflags '-m -m' ./*.go 2>&1 |less
+// Run all tests
+//   go test -v
+// Run one test and do allocation profiling
+//   go test -run=XXX -bench=IterativeISOTimestampLong -gcflags '-m' 2>&1 |less
+// Run a specific test by function name pattern
+//  go test -run=TestParsISOTimestamp
+//
+//  go test -run=XXX -bench=.
+//  go test -bench=. -benchmem -memprofile memprofile.out -cpuprofile cpuprofile.out
+//  go tool pprof -http=:8080 memprofile.out
+//  go tool pprof -http=:8080 cpuprofile.out
+
+// To generate a new RSA private/public key pair
+// openssl genrsa -out ./id_rsa 4096
+// openssl rsa -in ./id_rsa -pubout -out ./id_rsa.pub
+
+const (
+	bechmarkBytesPerOp int64 = 10
+)
+
+func init() {
+}
+
+// Get some lines
+func TestGetLines(t *testing.T) {
+
+	lines, err := getLines(10, "sample/1.txt")
+	if err != nil {
+		t.Fail()
+	}
+	t.Log("lines", lines)
+}
+
+// go test -run=XXX -bench=. -benchmem
+// BenchmarkGetLines-12    659.9 ns/op    15.15 MB/s    363 B/op    3 allocs/op
+func BenchmarkGetLines(b *testing.B) {
+
+	var lines []string
+	var err error
+
+	b.SetBytes(bechmarkBytesPerOp)
+	b.ReportAllocs()
+	b.SetParallelism(30)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lines, err = getLines(10, "sample/1.txt")
+		}
+	})
+
+	if len(lines) == 0 {
+		b.Fail()
+	}
+	if err != nil {
+		b.Fail()
+	}
+}
